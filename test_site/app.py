ba64fdf8c4cc57d968c43fc673eb6f5f83d4761f
MarkLark <mark.pittaway@mlit.net.au>
2017-01-30 19:29:46 +1100

    Updated README.rst, and for now we will use dstore.MemoryStore instead of dstore_mongo.MongoStore Until we get the framework set up and the website running, then we can test against the other storage types

--- test_site/app.py
+++ test_site/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 from .models import models
 from flask_dstore import API
-from dstore_mongo import MongoStore
+from dstore import MemoryStore
 
 
-def create_app( name = __name__, config = "tests.configs.unit_tests" ):
+def create_app( name = __name__, config = "tests.configs.unit_tests", store = MemoryStore ):
     app = Flask( name, static_url_path = "", static_folder = "web/app" )
     app.config.from_object( config )
-    api = API( MongoStore( models ), app )
+    api = API( store( models ), app )
 
     return app, api
